12e7d2083f735b553f8b4a973af125093303b5ac
ChillerDragon <chillerdragon@gmail.com>
2024-11-29 15:23:30 +0800

    fix nullterm omg

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-	uint8_t bytes[] = {'f', 0x01};
+	uint8_t bytes[] = {'f', 0x00};
 	Unpacker unpacker = unpacker_new(bytes, sizeof(bytes));
 	const char *str = unpacker_get_string(&unpacker);
 	printf("str '%s' \n", str);
